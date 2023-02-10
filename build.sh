809f3df289d47a36689b0467f96d187cd45df07a
AOIDUO <aoiduo@riseup.com>
2023-02-10 15:56:14 +0000

    build

---
+++ build.sh
@@ -0,0 +1,4 @@
+xelatex -shell-escape "/home/aoiduo/Documents/cv/main-zh.tex"
+xelatex -shell-escape "/home/aoiduo/Documents/cv/main-en.tex"
+mv main-zh.pdf "2023-02-周祺.pdf"
+mv main-en.pdf "2023-02-zhou-qi.pdf"
